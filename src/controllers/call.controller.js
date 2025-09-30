eaa69a22a492366103c5e4397c1137be2975ae9f
Apoorv Rathore <apoorv@bitmaxgroup.com>
2025-09-30 10:31:33 +0530

    Modify call.controller.js

--- src/controllers/call.controller.js
+++ src/controllers/call.controller.js
@@ -42,7 +42,6 @@ export async function getCallByFilter(req, res) {
       calls
     });
   } catch (err) {
-    console.error("getCallByFilter error:", err.message);
     return res.status(500).json({ error: err.message });
   }
 }
